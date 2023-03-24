c486447f65165cc609455daf069ab4a362c69a73
mayankbadika <mayank.badika@gmail.com>
2023-03-24 15:23:09 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", "CI3ZyOUvDJgsgzRENJvoJI13HTr5tjy6");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+const Base_URL = 'https://api.apilayer.com/exchangerates_data/latest';
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, settoCurrency] = useState()
+  useEffect(()=>{
+    fetch(Base_URL,requestOptions)
+      .then(res=> res.json())
+      .then(data=> {
+        const currency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        settoCurrency(currency)
+      })
+      .catch(error=>console.error(error));
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Convert</h1>
+    <CurrencyRow 
+    currencyOptions={currencyOptions} 
+    selectedCurrency={fromCurrency}
+    onChangeCurrency={e=>setFromCurrency(e.target.value)}
+    // changing state using this function . e is event from select
+    //https://upmostly.com/tutorials/react-onchange-events-with-examples
+    />
+    <div className='equals'> = </div>
+    <CurrencyRow 
+    currencyOptions={currencyOptions}
+    selectedCurrency={toCurrency}
+    onChangeCurrency={e=> settoCurrency(e.target.value)}
+    />
+    </>
+    
   );
 }
 
